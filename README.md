a2bf8a7b9bd6766cc4be6dce1d7de69fba33da54
nicolasfrechette <nicolasfrechette91@gmail.com>
2022-12-03 21:52:41 -0500

    Improved and cleaned the technical section

--- README.md
+++ README.md
@@ -5,13 +5,11 @@ You can contact me if you have any question
 
 Nicolas Frechette
 
-# Image attribution
+# Image sources
 <a href="https://iconscout.com/illustrations/developer-team" target="_blank">Developer Team Illustration</a> by <a href="https://iconscout.com/contributors/delesign" target="_blank">Delesign Graphics</a>
 
 <a href="https://iconscout.com/illustrations/man" target="_blank">Man developing website on desk Illustration</a> by <a href="https://iconscout.com/contributors/woobrodesign" target="_blank">WOOBRO LTD</a>
 
 <a href="https://iconscout.com/illustrations/software-engineer" target="_blank">Software engineer Illustration</a> by <a href="https://iconscout.com/contributors/delesign" target="_blank">Delesign Graphics</a>
 
-<a href="https://iconscout.com/illustrations/testing" target="_blank">Testing Illustration</a> by <a href="https://iconscout.com/contributors/delesign" target="_blank">Delesign Graphics</a>
-
-<a href="https://iconscout.com/illustrations/women" target="_blank">Women Web Developer with laptop Illustration</a> by <a href="https://iconscout.com/contributors/woobrodesign">WOOBRO LTD</a> on <a href="https://iconscout.com">IconScout</a>
+<a href="https://iconscout.com/illustrations/cloud-storage" target="_blank">Cloud Storage Illustration</a> by <a href="https://iconscout.com/contributors/woobrodesign" target="_blank">WOOBRO LTD</a>
--- src/app/skills.ts
+++ src/app/skills.ts
@@ -2,26 +2,21 @@ export const Skills = [
   {
     icon:'https://nicolasfrechette91.github.io/portfolio/assets/images/man-developing-website-on-desk.svg',
     name: 'Front-end Programming',
-    tech: ['Angular 15 (TypeScript)', 'React', 'jQuery', 'HTML5, CSS3, Bootstrap', 'Node.js', 'Consume API, Parse JSON']
+    tech: ['Angular 15 (TypeScript)', 'React', 'HTML5, CSS3, Bootstrap', 'Node.js', 'jQuery']
   },
   {
-    icon:'https://nicolasfrechette91.github.io/portfolio/assets/images/icon10.svg',
+  icon:'https://nicolasfrechette91.github.io/portfolio/assets/images/software-engineer.svg',
     name: 'Back-end Programming',
-    tech: ['Java (Spring Boot, Hibernate ORM)','C#', 'ASP.NET','RESTful API and Web Services return JSON', 'Linux Bash Shell']
+    tech: ['C#', 'RESTful API and Web Services', 'Java', 'ASP.NET', 'Python']
   },
   {
-    icon:'https://nicolasfrechette91.github.io/portfolio/assets/images/icon2.svg',
-    name: 'Mobile Application',
-    tech: ['Android Kotlin (Android Studio IDE)', 'iOS Swift (XCode IDE)', 'SQLite3']
-  },
-  {
-    icon:'https://nicolasfrechette91.github.io/portfolio/assets/images/icon13.svg',
+    icon:'https://nicolasfrechette91.github.io/portfolio/assets/images/cloud-storage.svg',
     name: 'Database',
-    tech: ['SQL Server Management Studio 17', 'MySQL 5.7', 'Oracle SQL Developer', 'Big Data Storage', 'MongoDB NoSQL']
+    tech: ['MySQL 8.0', 'Oracle SQL Developer', 'MongoDB NoSQL']
   },
   {
-    icon:'https://nicolasfrechette91.github.io/portfolio/assets/images/business.svg',
-    name: 'System Analysis',
-    tech: ['Software Development Life Cycle', 'System Context Diagram', 'Data Flow Diagrams', 'Microsoft Visio']
+    icon:'https://nicolasfrechette91.github.io/portfolio/assets/images/developer-team.svg',
+    name: 'Maintenance',
+    tech: ['Test', 'Debug', 'Create release branch', 'Deploy changes']
   },
 ];
