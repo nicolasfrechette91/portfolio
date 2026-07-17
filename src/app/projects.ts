8b50242b7b136618438e0a20321318c492cd7ec6
nicolasfrechette91 <nicolasfrechette91@gmail.com>
2026-07-16 22:28:06 -0400

    try to add a video to the website project

--- src/app/projects.ts
+++ src/app/projects.ts
@@ -17,7 +17,7 @@ export const Projects = [
     image: 'https://nicolasfrechette91.github.io/portfolio/assets/images/wedding_project.png',
     link: 'https://nicolasfrechette91.github.io/nico_fanny_wedding_2020/home/index.html',
     name: 'Wedding Website',
-    description: 'Creation from scratch of a website in preparation for my wedding. This was used to inform the guests of the location and gather all of the pictures in one location.',
+    description: 'Personalized UI design for a major event. The website was used to inform the event details as well as to create announcements, photo repository and souvenir details in one location.',
     languages: 'HTML · jQuery · CSS',
     linkFrench: 'https://nicolasfrechette91.github.io/nico_fanny_wedding_2020/home/index_fr.html',
     nameFrench: 'Site web pour mon mariage',
@@ -25,12 +25,12 @@ export const Projects = [
     languagesFrench: 'HTML · jQuery · CSS',
     dialog: {
       title: 'My Wedding Website',
-      description: 'This was built to preserve nice memories of my wedding and as a creative way to inform the guests.',
+      description: 'This website was built to preserve memories while keeping a personalized and creative style throughout the website.',
       tasks: ['Develop website and logic according to UI design made by my wife', 'Adjust the UI to fit on mobile', 'Structure and organize to be able to reuse components / styles when possible', 'Support 3 languages (English / French / Spanish)'],
       images:'',
-      video:'',
+      video:['https://nicolasfrechette91.github.io/portfolio/assets/videos/website.mp4'],
       titleFrench: 'Site internet de mon mariage',
-      descriptionFrench: "Cela a été construit pour préserver de beaux souvenirs de mon mariage et comme un moyen créatif d'informer les invités avec les nouvelles de l'événement.",
+      descriptionFrench: "Ce site Web a été créé pour préserver les souvenirs tout en conservant un style personnalisé et créatif.",
       tasksFrench: ["Développer le site Web en fonction du design réalisé par ma femme", "Ajuster l'interface utilisateur pour l'adapter au téléphone mobile", "Structurer et organiser pour pouvoir réutiliser les composants/styles lorsque cela était possible", "Traduction en 3 langues (Français/Anglais/Espagnol)"]
     }
   },
