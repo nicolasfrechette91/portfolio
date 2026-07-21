b386e5faa8c3b7300634b67a40080d091fe7b5aa
nicolasfrechette91 <nicolasfrechette91@gmail.com>
2026-07-20 20:49:57 -0400

    added personal project for level habit

--- src/app/projects.ts
+++ src/app/projects.ts
@@ -7,6 +7,19 @@ export interface DialogData {
 /* personal projects*/
 export const Projects = [
   {
+    id: 2,
+    github: 'https://github.com/nicolasfrechette91/LevelHabit',
+    image: 'https://nicolasfrechette91.github.io/portfolio/assets/images/levelHabit_login.png',
+    link: 'https://nicolasfrechette91.github.io/LevelHabit/',
+    linkFrench: 'https://nicolasfrechette91.github.io/LevelHabit/',
+    translationKey: 'projects.items.levelHabit',
+    dialog: {
+      translationKey: 'projects.items.levelHabit.dialog',
+      image: 'https://nicolasfrechette91.github.io/portfolio/assets/images/levelHabit_habits.png',
+      video: ''
+    }
+  },
+  {
     id: 1,
     github: 'https://github.com/nicolasfrechette91/nico_fanny_wedding_2020',
     image: 'https://nicolasfrechette91.github.io/portfolio/assets/images/wedding_project.png',
